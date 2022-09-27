7ebe5a5ddcf45d4e5da8e4e4581dfb6ee2fc5697
Thenicolaibulow <nixen.dyre@gmail.com>
2022-09-27 12:56:28 +0200

    bi-phase mark encoder now abstracted away as a seperate class/module

--- InterVox_Transmitter/interVox_Encoder.v
+++ InterVox_Transmitter/interVox_Encoder.v
@@ -1,3 +1,84 @@
+module bi_phase_encoder(
+  input   clock,
+  input   reset,
+  input   io_DATA_IN,
+  output  io_DATA_OUT,
+  input   io_NxtBit
+);
+`ifdef RANDOMIZE_REG_INIT
+  reg [31:0] _RAND_0;
+  reg [31:0] _RAND_1;
+`endif // RANDOMIZE_REG_INIT
+  reg  hasNone; // @[intervox_transmitter.scala 16:26]
+  reg  outReg; // @[intervox_transmitter.scala 17:26]
+  wire  _GEN_0 = ~io_DATA_IN | hasNone; // @[intervox_transmitter.scala 22:32 24:17 16:26]
+  assign io_DATA_OUT = outReg; // @[intervox_transmitter.scala 18:17]
+  always @(posedge clock) begin
+    if (reset) begin // @[intervox_transmitter.scala 16:26]
+      hasNone <= 1'h0; // @[intervox_transmitter.scala 16:26]
+    end else if (io_NxtBit) begin // @[intervox_transmitter.scala 20:28]
+      if (hasNone) begin // @[intervox_transmitter.scala 26:28]
+        hasNone <= 1'h0; // @[intervox_transmitter.scala 28:17]
+      end else begin
+        hasNone <= _GEN_0;
+      end
+    end
+    if (reset) begin // @[intervox_transmitter.scala 17:26]
+      outReg <= 1'h0; // @[intervox_transmitter.scala 17:26]
+    end else if (io_NxtBit) begin // @[intervox_transmitter.scala 20:28]
+      if (!(hasNone)) begin // @[intervox_transmitter.scala 26:28]
+        outReg <= ~outReg; // @[intervox_transmitter.scala 32:16]
+      end
+    end
+  end
+// Register and memory initialization
+`ifdef RANDOMIZE_GARBAGE_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_INVALID_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_REG_INIT
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+`define RANDOMIZE
+`endif
+`ifndef RANDOM
+`define RANDOM $random
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+  integer initvar;
+`endif
+`ifndef SYNTHESIS
+`ifdef FIRRTL_BEFORE_INITIAL
+`FIRRTL_BEFORE_INITIAL
+`endif
+initial begin
+  `ifdef RANDOMIZE
+    `ifdef INIT_RANDOM
+      `INIT_RANDOM
+    `endif
+    `ifndef VERILATOR
+      `ifdef RANDOMIZE_DELAY
+        #`RANDOMIZE_DELAY begin end
+      `else
+        #0.002 begin end
+      `endif
+    `endif
+`ifdef RANDOMIZE_REG_INIT
+  _RAND_0 = {1{`RANDOM}};
+  hasNone = _RAND_0[0:0];
+  _RAND_1 = {1{`RANDOM}};
+  outReg = _RAND_1[0:0];
+`endif // RANDOMIZE_REG_INIT
+  `endif // RANDOMIZE
+end // initial
+`ifdef FIRRTL_AFTER_INITIAL
+`FIRRTL_AFTER_INITIAL
+`endif
+`endif // SYNTHESIS
+endmodule
 module interVox_Encoder(
   input   clock,
   input   reset,
@@ -15,55 +96,61 @@ module interVox_Encoder(
   reg [31:0] _RAND_0;
   reg [31:0] _RAND_1;
   reg [31:0] _RAND_2;
-  reg [31:0] _RAND_3;
 `endif // RANDOMIZE_REG_INIT
-  reg [1:0] current_state; // @[intervox_transmitter.scala 27:30]
-  reg [7:0] BiPhase_CLK_CNTR; // @[intervox_transmitter.scala 29:33]
-  reg  hasOne; // @[intervox_transmitter.scala 30:23]
-  reg  DATA_OUT_REG; // @[intervox_transmitter.scala 32:29]
-  wire [7:0] _BiPhase_CLK_CNTR_T_1 = BiPhase_CLK_CNTR + 8'h1; // @[intervox_transmitter.scala 44:42]
-  wire  _GEN_0 = io_SDATA_IN | hasOne; // @[intervox_transmitter.scala 58:37 60:20 30:23]
-  wire  _GEN_1 = hasOne ? DATA_OUT_REG : ~DATA_OUT_REG; // @[intervox_transmitter.scala 62:31 63:26 68:26]
-  wire  _GEN_2 = hasOne ? 1'h0 : _GEN_0; // @[intervox_transmitter.scala 62:31 64:20]
-  wire  _GEN_3 = 2'h2 == current_state ? _GEN_2 : hasOne; // @[intervox_transmitter.scala 30:23 47:29]
-  wire  _GEN_4 = 2'h2 == current_state ? _GEN_1 : DATA_OUT_REG; // @[intervox_transmitter.scala 32:29 47:29]
-  assign io_DATA_O = DATA_OUT_REG; // @[intervox_transmitter.scala 33:13]
-  assign io_LRCLK_O = io_LRCLK_IN; // @[intervox_transmitter.scala 37:14]
-  assign io_BCLK_O = io_BCLK_IN; // @[intervox_transmitter.scala 36:13]
-  assign io_MCLK_O = io_MCLK_IN; // @[intervox_transmitter.scala 35:13]
-  assign io_SDATA_O = io_SDATA_IN; // @[intervox_transmitter.scala 38:14]
+  wire  bi_phase_enc_clock; // @[intervox_transmitter.scala 58:28]
+  wire  bi_phase_enc_reset; // @[intervox_transmitter.scala 58:28]
+  wire  bi_phase_enc_io_DATA_IN; // @[intervox_transmitter.scala 58:28]
+  wire  bi_phase_enc_io_DATA_OUT; // @[intervox_transmitter.scala 58:28]
+  wire  bi_phase_enc_io_NxtBit; // @[intervox_transmitter.scala 58:28]
+  reg [1:0] current_state; // @[intervox_transmitter.scala 53:30]
+  reg [7:0] BiPhase_CLK_CNTR; // @[intervox_transmitter.scala 55:33]
+  reg  DATA_OUT_REG; // @[intervox_transmitter.scala 57:29]
+  wire [7:0] _BiPhase_CLK_CNTR_T_1 = BiPhase_CLK_CNTR + 8'h1; // @[intervox_transmitter.scala 72:42]
+  wire  _GEN_1 = 2'h2 == current_state & io_SDATA_IN; // @[intervox_transmitter.scala 65:27 75:29 86:35]
+  wire  _GEN_2 = 2'h2 == current_state ? bi_phase_enc_io_DATA_OUT : DATA_OUT_REG; // @[intervox_transmitter.scala 75:29 87:24 57:29]
+  wire  _GEN_4 = 2'h1 == current_state ? 1'h0 : 2'h2 == current_state; // @[intervox_transmitter.scala 66:26 75:29]
+  wire  _GEN_5 = 2'h1 == current_state ? 1'h0 : _GEN_1; // @[intervox_transmitter.scala 65:27 75:29]
+  wire  _GEN_8 = 2'h0 == current_state ? 1'h0 : _GEN_4; // @[intervox_transmitter.scala 66:26 75:29]
+  wire  _GEN_9 = 2'h0 == current_state ? 1'h0 : _GEN_5; // @[intervox_transmitter.scala 65:27 75:29]
+  bi_phase_encoder bi_phase_enc ( // @[intervox_transmitter.scala 58:28]
+    .clock(bi_phase_enc_clock),
+    .reset(bi_phase_enc_reset),
+    .io_DATA_IN(bi_phase_enc_io_DATA_IN),
+    .io_DATA_OUT(bi_phase_enc_io_DATA_OUT),
+    .io_NxtBit(bi_phase_enc_io_NxtBit)
+  );
+  assign io_DATA_O = DATA_OUT_REG; // @[intervox_transmitter.scala 60:13]
+  assign io_LRCLK_O = io_LRCLK_IN; // @[intervox_transmitter.scala 63:14]
+  assign io_BCLK_O = io_BCLK_IN; // @[intervox_transmitter.scala 62:13]
+  assign io_MCLK_O = io_MCLK_IN; // @[intervox_transmitter.scala 61:13]
+  assign io_SDATA_O = io_SDATA_IN; // @[intervox_transmitter.scala 64:14]
+  assign bi_phase_enc_clock = clock;
+  assign bi_phase_enc_reset = reset;
+  assign bi_phase_enc_io_DATA_IN = BiPhase_CLK_CNTR == 8'h8 & _GEN_9; // @[intervox_transmitter.scala 65:27 73:35]
+  assign bi_phase_enc_io_NxtBit = BiPhase_CLK_CNTR == 8'h8 & _GEN_8; // @[intervox_transmitter.scala 66:26 73:35]
   always @(posedge clock) begin
-    if (reset) begin // @[intervox_transmitter.scala 27:30]
-      current_state <= 2'h0; // @[intervox_transmitter.scala 27:30]
-    end else if (BiPhase_CLK_CNTR == 8'h3) begin // @[intervox_transmitter.scala 45:35]
-      if (2'h0 == current_state) begin // @[intervox_transmitter.scala 47:29]
-        current_state <= 2'h2; // @[intervox_transmitter.scala 50:25]
-      end else if (2'h1 == current_state) begin // @[intervox_transmitter.scala 47:29]
-        current_state <= 2'h2; // @[intervox_transmitter.scala 53:25]
+    if (reset) begin // @[intervox_transmitter.scala 53:30]
+      current_state <= 2'h0; // @[intervox_transmitter.scala 53:30]
+    end else if (BiPhase_CLK_CNTR == 8'h8) begin // @[intervox_transmitter.scala 73:35]
+      if (2'h0 == current_state) begin // @[intervox_transmitter.scala 75:29]
+        current_state <= 2'h2; // @[intervox_transmitter.scala 78:25]
+      end else if (2'h1 == current_state) begin // @[intervox_transmitter.scala 75:29]
+        current_state <= 2'h2; // @[intervox_transmitter.scala 81:25]
       end
     end
-    if (reset) begin // @[intervox_transmitter.scala 29:33]
-      BiPhase_CLK_CNTR <= 8'h0; // @[intervox_transmitter.scala 29:33]
-    end else if (BiPhase_CLK_CNTR == 8'h3) begin // @[intervox_transmitter.scala 45:35]
-      BiPhase_CLK_CNTR <= 8'h0; // @[intervox_transmitter.scala 72:22]
+    if (reset) begin // @[intervox_transmitter.scala 55:33]
+      BiPhase_CLK_CNTR <= 8'h0; // @[intervox_transmitter.scala 55:33]
+    end else if (BiPhase_CLK_CNTR == 8'h8) begin // @[intervox_transmitter.scala 73:35]
+      BiPhase_CLK_CNTR <= 8'h0; // @[intervox_transmitter.scala 90:22]
     end else begin
-      BiPhase_CLK_CNTR <= _BiPhase_CLK_CNTR_T_1; // @[intervox_transmitter.scala 44:22]
+      BiPhase_CLK_CNTR <= _BiPhase_CLK_CNTR_T_1; // @[intervox_transmitter.scala 72:22]
     end
-    if (reset) begin // @[intervox_transmitter.scala 30:23]
-      hasOne <= 1'h0; // @[intervox_transmitter.scala 30:23]
-    end else if (BiPhase_CLK_CNTR == 8'h3) begin // @[intervox_transmitter.scala 45:35]
-      if (!(2'h0 == current_state)) begin // @[intervox_transmitter.scala 47:29]
-        if (!(2'h1 == current_state)) begin // @[intervox_transmitter.scala 47:29]
-          hasOne <= _GEN_3;
-        end
-      end
-    end
-    if (reset) begin // @[intervox_transmitter.scala 32:29]
-      DATA_OUT_REG <= 1'h0; // @[intervox_transmitter.scala 32:29]
-    end else if (BiPhase_CLK_CNTR == 8'h3) begin // @[intervox_transmitter.scala 45:35]
-      if (!(2'h0 == current_state)) begin // @[intervox_transmitter.scala 47:29]
-        if (!(2'h1 == current_state)) begin // @[intervox_transmitter.scala 47:29]
-          DATA_OUT_REG <= _GEN_4;
+    if (reset) begin // @[intervox_transmitter.scala 57:29]
+      DATA_OUT_REG <= 1'h0; // @[intervox_transmitter.scala 57:29]
+    end else if (BiPhase_CLK_CNTR == 8'h8) begin // @[intervox_transmitter.scala 73:35]
+      if (!(2'h0 == current_state)) begin // @[intervox_transmitter.scala 75:29]
+        if (!(2'h1 == current_state)) begin // @[intervox_transmitter.scala 75:29]
+          DATA_OUT_REG <= _GEN_2;
         end
       end
     end
@@ -109,9 +196,7 @@ initial begin
   _RAND_1 = {1{`RANDOM}};
   BiPhase_CLK_CNTR = _RAND_1[7:0];
   _RAND_2 = {1{`RANDOM}};
-  hasOne = _RAND_2[0:0];
-  _RAND_3 = {1{`RANDOM}};
-  DATA_OUT_REG = _RAND_3[0:0];
+  DATA_OUT_REG = _RAND_2[0:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
--- InterVox_Transmitter/src/main/scala/intervox_transmitter.scala
+++ InterVox_Transmitter/src/main/scala/intervox_transmitter.scala
@@ -7,6 +7,32 @@ import chisel3.util._
     which carries both (up to) 24-bit audio, along side DSP control data.
   */
  
+class bi_phase_encoder() extends Module {
+  val io = IO(new Bundle {
+    val DATA_IN   = Input (UInt(1.W))
+    val DATA_OUT  = Output(UInt(1.W))
+    val NxtBit    = Input (UInt(1.W))
+  })
+    val hasNone = RegInit(0.U(1.W))
+    val outReg  = RegInit(0.U(1.W))
+    io.DATA_OUT := outReg
+
+    when(io.NxtBit === 1.U){  
+      // If we recieve a bit from Input:
+      when(io.DATA_IN === 0.U) {
+        // Then don't change for one cycle
+        hasNone := 1.U
+      }
+      when(hasNone === 1.U){
+        outReg := outReg
+        hasNone := 0.U
+      }
+      .otherwise{
+        // Otherwise, flip
+        outReg := ~outReg
+      }
+    }
+}
 
 class interVox_Encoder(width: UInt) extends Module {
   val io = IO(new Bundle {
@@ -27,22 +53,24 @@ class interVox_Encoder(width: UInt) extends Module {
   val current_state = RegInit(state_Reset)  
   // Bi-phase clock counter register.
   val BiPhase_CLK_CNTR = RegInit(0.U(8.W))
-  val hasOne = RegInit(0.U(1.W))
   // Output register
   val DATA_OUT_REG = RegInit(0.U(1.W))
+  val bi_phase_enc = Module(new bi_phase_encoder())
+  
   io.DATA_O := DATA_OUT_REG
-
   io.MCLK_O := io.MCLK_IN
   io.BCLK_O := io.BCLK_IN 
   io.LRCLK_O := io.LRCLK_IN 
   io.SDATA_O := io.SDATA_IN
+  bi_phase_enc.io.DATA_IN := 0.U
+  bi_phase_enc.io.NxtBit := 0.U
 
   // Clock internal logic off of the external I2S MCLK, to keep everything synced.
   withClock(io.MCLK_IN){
 
     // First we need a clock divider to provide the 2 x BCLK, needed for the bi-phase mark encoding.
     BiPhase_CLK_CNTR := BiPhase_CLK_CNTR + 1.U
-    when(BiPhase_CLK_CNTR === 3.U){
+    when(BiPhase_CLK_CNTR === 8.U){
 
       switch(current_state) {
 
@@ -53,20 +81,12 @@ class interVox_Encoder(width: UInt) extends Module {
           current_state := state_Transmit
         }
         is(state_Transmit) {
-          // So for bi-phase mark:
-          // If we recieve a bit from SDATA:
-          when(io.SDATA_IN === 1.U) {
-            // Then don't change for one cycle
-            hasOne := 1.U
-          }
-          when(hasOne === 1.U){
-            DATA_OUT_REG := DATA_OUT_REG
-            hasOne := 0.U
-          }
-          .otherwise{
-            // Otherwise, flip with clock.
-            DATA_OUT_REG := ~DATA_OUT_REG
-          }
+          // Get the bi-phase encoder ready
+          bi_phase_enc.io.NxtBit := 1.U
+          // Populate output register with bi-phase encoded data. 
+          bi_phase_enc.io.DATA_IN := io.SDATA_IN
+          // Recieve encoded data, and clock it out on data_out
+          DATA_OUT_REG := bi_phase_enc.io.DATA_OUT 
       }
     }
     BiPhase_CLK_CNTR := 0.U
